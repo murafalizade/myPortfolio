5f27e0ae55234f4a8c316eb61206c59c54e19ad6
Murad <muradaliyev2229@gmail.com>
2024-05-24 17:22:14 +0400

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,13 +40,13 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
+    title: "Murad Aliyev - Software Developer",
+    fullName: "Murad Aliyev",
+    email: "muradaliyev2229@gmail.com",
   },
   hero: {
-    name: "John Doe",
-    p: ["I develop 3D visuals, user", "interfaces and web applications"],
+    name: "Murad Aliyev",
+    p: ["My position consist of making project which" , "exist front-end components and back-end API"],
   },
   contact: {
     p: "Get in touch",
@@ -66,12 +66,8 @@ export const config: TConfig = {
   sections: {
     about: {
       p: "Introduction",
-      h2: "Overview.",
-      content: `I'm a skilled software developer with experience in TypeScript and
-      JavaScript, and expertise in frameworks like React, Node.js, and
-      Three.js. I'm a quick learner and collaborate closely with clients to
-      create efficient, scalable, and user-friendly solutions that solve
-      real-world problems. Let's work together to bring your ideas to life!`,
+      h2: "Services",
+      content: ``,
     },
     experience: {
       p: "What I have done so far",
